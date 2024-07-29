b633cd7ec2e0b5531622c74ec9ce0639d128d4bf
the9ova <mohammednassr82@gmail.com>
2024-07-29 16:55:51 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,237 @@
+from rich.panel import Panel as Ch 
+import os
+import os,requests,sys,time,datetime
+now = datetime.datetime.today()
+an = datetime.datetime.now()
+an2 = datetime.datetime(2029,7,25,00,0)
+hours = (now.hour)
+if an > an2 or an == an2:
+ print('\033[1;31mThe Date : '+ str(an))
+ print('\033[1;31mThe Exp Date : '+ str(an2))
+ time.sleep(1)
+ clock_emoji = ['🕛', '🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚']
+# exit(0)
+else: 
+ print('𓏳'*60)
+ print('')
+ print('𓏳'*60)
+ print('\t\n\033[1;31m 𓆩 نــمـــرود⏤‌🇮🇶⃤ 𓆪 ‏ الـــمـــطـــوࢪ   ')
+ print('\n\033[1;37m جـاࢪي تـشـغـيـل الاداة ♡ ......')
+ DeKo = '''''''''''print('𓏳'50)'''''''''''
+a1 = '\x1b[1;31m'  # أحمر
+a2 = '\x1b[1;34m'  # أزرق
+a3 = '\x1b[1;32m'  # أخضر
+a4 = '\x1b[1;33m'  # أصفر
+a5 = '\x1b[38;5;208m'  # برتقالي
+a6 = '\x1b[38;5;5m'  # أرجواني
+a7 = '\x1b[38;5;13m'  # وردي
+a8 = '\x1b[1;30m'  # أسود
+a9 = '\x1b[1;37m'  # أبيض
+a10 = '\x1b[38;5;52m'  # بني
+a11 = '\x1b[38;5;8m'  # رمادي
+a12 = '\x1b[38;5;220m'  # ذهبي
+a13 = '\x1b[38;5;7m'  # فضي
+a14 = '\x1b[38;5;153m'  # أزرق فاتح
+a15 = '\x1b[38;5;18m'  # أزرق داكن
+a16 = '\x1b[38;5;48m'  # أخضر فاتح
+a17 = '\x1b[38;5;22m'  # أخضر داكن
+a18 = '\x1b[38;5;196m'  # أحمر فاتح
+a19 = '\x1b[38;5;88m'  # أحمر داكن
+a20 = '\x1b[38;5;226m'  # أصفر فاتح
+a21 = '\x1b[38;5;136m'  # أصفر داكن
+a22 = '\x1b[38;5;216m'  # برتقالي فات
+a23 = '\x1b[38;5;166m'  # برتقالي داكن
+a24 = '\x1b[38;5;234m'  # أرجواني فاتح
+a25 = '\x1b[38;5;91m'  # أرجواني داكن
+a26 = '\x1b[38;5;205m'  # وردي فاتح
+a27 = '\x1b[38;5;161m'  # وردي داكن
+a28 = '\x1b[38;5;236m'  # أسود فاتح
+a29 = '\x1b[38;5;233m'  # أسود داكن
+a30 = '\x1b[38;5;255m'  # أبيض فاتح
+a31 = '\x1b[38;5;231m'  # أبيض داكن
+a32 = '\x1b[38;5;180m'  # بني فاتح
+a33 = '\x1b[38;5;94m'  # بني داكن
+a34 = '\x1b[38;5;252m'  # رمادي فاتح
+a35 = '\x1b[38;5;246m'  # رمادي داكن
+a36 = '\x1b[38;5;228m'  # ذهبي فاتح
+a37 = '\x1b[38;5;172m'  # ذهبي داكن
+a38 = '\x1b[38;5;188m'  # فضي فاتح
+a39 = '\x1b[38;5;247m'  # فضي داكن
+a40 = '\x1b[38;5;117m'  # أزرق سماوي
+#------------------colors---------------#
+B="\033[1;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # White
+E = "\033[0;90m" #رمادي
+#------------------logo---------------------#
+def logo():
+    print(f'{G}-{R}-'*29)	
+    
+    print(f"""
+──────────────────────────────────────────────────────
+    """)
+    
+    print(f'{G}-{R}-'*29)
+    print ('  '+Y+'['+P+'*'+Y+']'+C+'Code By :'+W+' zzkgz𓆪™✓🇮🇶')
+    print ('  '+Y+'['+P+'*'+Y+']'+C+'Channel :'+W+' @zzkgz')
+    print(f'{G}-{R}-'*29)
+logo()
+dead = (C+"""▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬~zzkgz𓆪~▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬""")
+
+try:
+ from cfonts import render, say
+except:
+ os.system('pip install python-cfonts')
+import requests
+def send_follow(zzkgz,zzkgz1,coo1,coo2):
+    HOSAM=input(a16+'مـعـرف الـحـسـاب الـي تـريـد تـرشـق لـه: ')
+    cookies = {
+    '_ga': 'GA1.2.379003127.1700346804',
+    '_gid': 'GA1.2.2030621174.1700346804',
+    coo1: coo2,
+    }
+    headers = {
+    'authority': 'instamoda.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'referer': 'https://instamoda.org/tools',
+    'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-M317F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+    }
+    response = requests.get('https://instamoda.org/tools/send-follower', cookies=cookies, headers=headers)
+    for cookie_name, cookie_value in response.cookies.items():pass
+    cookies = {
+    '_ga': 'GA1.2.379003127.1700346804',
+    '_gid': 'GA1.2.2030621174.1700346804',
+    cookie_name:cookie_value,
+    }
+    headers = {
+    'authority': 'instamoda.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'content-type': 'application/x-www-form-urlencoded',
+    'origin': 'https://instamoda.org',
+    'referer': 'https://instamoda.org/tools/send-follower',
+    'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-M317F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+    }
+    params = {
+    'formType': 'findUserID',
+    }
+    data = {
+    'username': HOSAM,
+    }
+    response = requests.post('https://instamoda.org/tools/send-follower', params=params, cookies=cookies, headers=headers, data=data)
+    id=response.text.split('<input type="hidden" name="userID" value="')[1].split('"')[0]
+    cookies = {
+    '_ga': 'GA1.2.379003127.1700346804',
+    '_gid': 'GA1.2.2030621174.1700346804',
+    cookie_name:cookie_value,
+    }
+    headers = {
+    'authority': 'instamoda.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'referer': 'https://instamoda.org/tools/send-follower',
+    'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-M317F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+}
+    response = requests.get(f'https://instamoda.org/tools/send-follower/{id}', cookies=cookies, headers=headers)
+    cookies = {
+    '_ga': 'GA1.2.379003127.1700346804',
+    '_gid': 'GA1.2.2030621174.1700346804',
+    cookie_name:cookie_value,
+    }
+    headers = {
+    'authority': 'instamoda.org',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'origin': 'https://instamoda.org',
+    'referer': f'https://instamoda.org/tools/send-follower/{id}',
+    'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-M317F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+    }
+    params = {
+    'formType': 'send',
+    }
+    data = {
+    'adet': '300',
+    'userID': id,
+    '	userName': HOSAM,
+    }
+    response = requests.post(
+    f'https://instamoda.org/tools/send-follower/{id}',
+    params=params,
+    cookies=cookies,
+    headers=headers,
+    data=data,
+    )
+    if response.json()['status']=='success':
+        print(f' تـم الـرشـق غـيـر بـاسـورد الـحـسـاب بـعـد 3 دقـائـق')
+zzkgz=input(a16+'اادخـل مـعـرف حـسـابـك الـوهـمـي : ')
+
+zzkgz1=input(a16+'ادخـل بـاسـورد حـسـابـك الـوهـمـي : ')
+headers = {
+    'authority': 'instamoda.org',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'origin': 'https://instamoda.org',
+    'referer': 'https://instamoda.org/login',
+    'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-M317F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+}
+params = ''
+data = {
+    'username': zzkgz,
+    'password': zzkgz1,
+    'userid': '',
+    'antiForgeryToken': '92e040589f9f0237f5ddd02297bbcf92',
+}
+response = requests.post('https://instamoda.org/login', params=params, headers=headers, data=data)
+if response.json()['status']=='success':
+    for cookie_name, cookie_value in response.cookies.items():pass
+    send_follow(zzkgz,zzkgz1,cookie_name,cookie_value)
+else:
+    print(response.text)
